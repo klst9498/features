3a1f33efc7c0616df7b8cf8432867334d30e10df
Staudenmaier, Klaus <klaus.Staudenmaier@elektrobit.com>
2025-03-30 17:42:53 +0200

    Add gstreamer install script for basler shared Memory streaming

---
+++ gstreamer/install.sh
@@ -0,0 +1,26 @@
+# Install required system dependencies for .deb file
+RUN apt update && apt install -y 
+    libgl1 \
+    ffmpeg \
+    libsm6 \
+    libxext6
+    gstreamer1.0-tools \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-ugly \
+    gstreamer1.0-libav \
+    python3-gst-1.0 \
+    python3-dev \
+    libffi-dev \
+    libcairo2 \
+    libcairo2-dev \
+    pkg-config \
+    gobject-introspection \
+    libgirepository1.0-dev \
+    python3-gi \
+    python3-gi-cairo \
+    gir1.2-gtk-3.0 \
+    gobject-introspection \
+    libcairo2-dev \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
