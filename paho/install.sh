ac12a9c994d09e2eaf59a92a62b1bd302b7f6cde
Staudenmaier, Klaus <klaus.Staudenmaier@elektrobit.com>
2025-09-02 18:27:19 +0200

    fix paho install

--- paho/install.sh
+++ paho/install.sh
@@ -4,7 +4,6 @@ cd /tmp/paho.mqtt.c
 cmake -Bbuild -H. -DPAHO_WITH_SSL=TRUE 
 cmake --build build/ --target install
 ldconfig
-rm -rf /tmp/paho.mqtt.c
 
 # === Build and install Paho MQTT C++ library ===
 git clone https://github.com/eclipse/paho.mqtt.cpp.git /tmp/paho.mqtt.cpp
@@ -13,3 +12,4 @@ cmake -Bbuild -H.
 cmake --build build/ --target install
 ldconfig
 rm -rf /tmp/paho.mqtt.cpp
+rm -rf /tmp/paho.mqtt.c
