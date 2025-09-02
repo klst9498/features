2ed30857a05b2257475e7fb04075f5de92e91e47
Staudenmaier, Klaus <klaus.Staudenmaier@elektrobit.com>
2025-09-02 15:56:44 +0200

    Add paho Mqtt

---
+++ paho/install.sh
@@ -0,0 +1,15 @@
+# === Build and install Paho MQTT C library ===
+git clone https://github.com/eclipse/paho.mqtt.c.git /tmp/paho.mqtt.c
+cd /tmp/paho.mqtt.c 
+cmake -Bbuild -H. -DPAHO_WITH_SSL=TRUE 
+cmake --build build/ --target install
+ldconfig
+rm -rf /tmp/paho.mqtt.c
+
+# === Build and install Paho MQTT C++ library ===
+git clone https://github.com/eclipse/paho.mqtt.cpp.git /tmp/paho.mqtt.cpp
+cd /tmp/paho.mqtt.cpp
+cmake -Bbuild -H.
+cmake --build build/ --target install
+ldconfig
+rm -rf /tmp/paho.mqtt.cpp
