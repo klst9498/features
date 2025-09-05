dc5886d698caf6b824db741cd3977660e0b53c7a
Klaus Staudenmaier <48551788+klst9498@users.noreply.github.com>
2025-09-05 15:02:56 +0200

    Update install.sh

--- paho/install.sh
+++ paho/install.sh
@@ -2,14 +2,14 @@
 git clone https://github.com/eclipse/paho.mqtt.c.git /tmp/paho.mqtt.c
 cd /tmp/paho.mqtt.c 
 cmake -Bbuild -H. -DPAHO_WITH_SSL=TRUE 
-sudo cmake --build build/ --target install
-sudo ldconfig
+cmake --build build/ --target install
+ldconfig
 
 # === Build and install Paho MQTT C++ library ===
 git clone https://github.com/eclipse/paho.mqtt.cpp.git /tmp/paho.mqtt.cpp
 cd /tmp/paho.mqtt.cpp
 cmake -Bbuild -H.
-sudo cmake --build build/ --target install
-sudo ldconfig
+cmake --build build/ --target install
+ldconfig
 rm -rf /tmp/paho.mqtt.cpp
 rm -rf /tmp/paho.mqtt.c
