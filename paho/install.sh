c619eb00490a03d7418c5c2c54c8675e3d9434c0
Klaus Staudenmaier <48551788+klst9498@users.noreply.github.com>
2025-09-05 14:57:33 +0200

    Add sudo to the install script

--- paho/install.sh
+++ paho/install.sh
@@ -2,14 +2,14 @@
 git clone https://github.com/eclipse/paho.mqtt.c.git /tmp/paho.mqtt.c
 cd /tmp/paho.mqtt.c 
 cmake -Bbuild -H. -DPAHO_WITH_SSL=TRUE 
-cmake --build build/ --target install
-ldconfig
+sudo cmake --build build/ --target install
+sudo ldconfig
 
 # === Build and install Paho MQTT C++ library ===
 git clone https://github.com/eclipse/paho.mqtt.cpp.git /tmp/paho.mqtt.cpp
 cd /tmp/paho.mqtt.cpp
 cmake -Bbuild -H.
-cmake --build build/ --target install
-ldconfig
+sudo cmake --build build/ --target install
+sudo ldconfig
 rm -rf /tmp/paho.mqtt.cpp
 rm -rf /tmp/paho.mqtt.c
