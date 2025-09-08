c821970eb7eee1c25b1267e1251b545625c9f8bf
max muster <48551788+klst9498@users.noreply.github.com>
2025-09-08 16:35:54 +0200

    Update install.sh

--- poetry/install.sh
+++ poetry/install.sh
@@ -3,3 +3,4 @@ USER=${USERNAME}
 apt update && apt install -y python3
 export POETRY_HOME="/home/$USER/.local" && \
 curl -sSL https://install.python-poetry.org | python3 -
+export PATH="/home/$USERNAME/.local/bin:$PATH"
