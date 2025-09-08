600824a8885a969ed3064c44ec504677c3624070
max muster <48551788+klst9498@users.noreply.github.com>
2025-09-08 16:27:01 +0200

    Update install.sh

--- poetry/install.sh
+++ poetry/install.sh
@@ -1,4 +1,5 @@
 # Install Poetry
+USER=${USERNAME}
 apt update && apt install -y python3
-export POETRY_HOME="/home/vscode/.local" && \
+export POETRY_HOME="/home/$USER/.local" && \
 curl -sSL https://install.python-poetry.org | python3 -
